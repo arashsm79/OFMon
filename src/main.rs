e3eb4f196faaf7b77b3ca23a2d7eeb44137cf71d
Arash Sal Moslehian <arashsm79@yahoo.com>
2022-08-10 11:02:06 +0000

    Generalize CT calculations

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::sync::Arc;
 use std::thread::sleep;
 use std::time::Duration;
 
+use embedded_hal_0_2_7::adc::OneShot;
 use embedded_svc::http::server::registry::Registry;
 use embedded_svc::http::server::{Request, Response};
 use embedded_svc::http::SendStatus;
@@ -11,16 +12,17 @@ use embedded_svc::io::Read as SvcRead;
 use embedded_svc::ipv4::{Ipv4Addr, Mask, RouterConfiguration, Subnet};
 use embedded_svc::wifi::Wifi;
 use embedded_svc::wifi::{AccessPointConfiguration, ApIpStatus, ApStatus, AuthMethod, Status};
+use esp_idf_hal::adc::{Atten11dB, PoweredAdc, ADC1};
+use esp_idf_hal::gpio::{Gpio34, Gpio35, Pins};
 use esp_idf_svc::http::server::EspHttpServer;
 use esp_idf_svc::netif::EspNetifStack;
 use esp_idf_svc::nvs::EspDefaultNvs;
 use esp_idf_svc::nvs_storage::EspNvsStorage;
-use esp_idf_svc::sntp;
+
 use esp_idf_svc::sysloop::EspSysLoopStack;
 use esp_idf_svc::wifi::EspWifi;
 use esp_idf_sys::esp;
 
-use embedded_hal_0_2_7::adc::OneShot;
 use esp_idf_hal::adc;
 use esp_idf_hal::prelude::Peripherals;
 
@@ -38,7 +40,32 @@ const DC_VOLTAGE: [u16; 3] = [1892; 3];
 const DC_CURRENT: [u16; 3] = [1635; 3];
 const CURRENT_SCALE: [f32; 3] = [102.0; 3]; //111.1;
 const VOLTAGE_SCALE: [f32; 3] = [232.5; 3];
-const MAX_SAMPLES: usize = 120;
+const MAX_SAMPLES: usize = 300;
+const AC_PHASE: u8 = 1;
+const ADC_COUNT: u32 = 4095;
+
+const SAMPLE_ACCUMULATOR: [f32; MAX_SAMPLES] = [0.0; MAX_SAMPLES];
+
+struct VoltagePin {
+    pin: Gpio34<Atten11dB<ADC1>>,
+    vcal: f32,
+    phase_cal: f32,
+    offset_v: u32,
+}
+
+struct CurrentPin {
+    pin: Gpio35<Atten11dB<ADC1>>,
+    ical: f32,
+    offset_i: u32,
+}
+
+struct CT {
+    current_pin: CurrentPin,
+    voltage_pin: VoltagePin,
+    current_samples: [f32; MAX_SAMPLES],
+    voltage_samples: [f32; MAX_SAMPLES],
+    sample_count: usize,
+}
 
 #[allow(dead_code)]
 static ACCESS_TOKEN: String = String::new();
@@ -55,29 +82,9 @@ fn main() -> anyhow::Result<()> {
     let _fs_conf = init_littlefs_storage()?;
     info!("Initialized and mounted littlefs storage.");
 
-    {
-        let mut file = fs::OpenOptions::new()
-            .write(true)
-            .create(true)
-            .open("/littlefs/logfile")?;
-        info!("Opened file for writing.");
-        file.write_all(b"Hello Wonderland!\n")?;
-        info!("Write complete.");
-    }
-    {
-        let mut file = fs::OpenOptions::new()
-            .read(true)
-            .write(true)
-            .open("/littlefs/logfile")?;
-        info!("Opened file for reading.");
-        let mut contents = String::new();
-        file.read_to_string(&mut contents)?;
-        info!("Read complete.");
-        println!("{}", contents);
-    }
-
     // Initialize NVS storage
-    let (default_nvs, keystore) = init_nvs_storage()?;
+    let (default_nvs, _keystore) = init_nvs_storage()?;
+    info!("Initialized default NVS storage.");
 
     // SSID and password for the Wifi access point.
     let mut ap_ssid: String = String::new();
@@ -88,25 +95,21 @@ fn main() -> anyhow::Result<()> {
     let _wifi = init_access_point(&ap_ssid, ap_password, default_nvs)?;
     info!("Initialized Wifi.");
 
-    let _web_server = init_web_server(keystore.clone())?;
+    let _web_server = init_web_server()?;
     info!("Initialized Web Server.");
 
     // Initilize peripherals and pins
     let peripherals = Peripherals::take().unwrap();
-    let pins = peripherals.pins;
+    let mut pins = peripherals.pins;
 
     // Initilize ADC
-    let mut single_phase_current_pin = pins.gpio35.into_analog_atten_11db()?;
-    info!("Initialized ADC1 pin: GPIO35.");
-    let mut single_phase_voltage_pin = pins.gpio34.into_analog_atten_11db()?;
-    info!("Initialized ADC1 pin: GPIO34.");
+
     let mut powered_adc1 = adc::PoweredAdc::new(
         peripherals.adc1,
         adc::config::Config::new().calibration(false),
     )?;
-    info!("Initialized ADC1.");
+    let mut cts = init_adc(pins)?;
 
-    //
     let ac_phase = 1;
     let mut current_samples = [0.0; MAX_SAMPLES];
     let mut voltage_samples = [0.0; MAX_SAMPLES];
@@ -115,33 +118,216 @@ fn main() -> anyhow::Result<()> {
     loop {
         // Read current and voltage values, calibrate them
         // and add them to the samples array
-        let raw_current_reading =
-            powered_adc1.read(&mut single_phase_current_pin).unwrap() - DC_CURRENT[ac_phase];
-        let current_reading =
-            CURRENT_SCALE[ac_phase] * ((raw_current_reading as f32 * 3.3) / 4095.0);
+        for ct in &mut cts {
+            ct.calculate_energy(&mut powered_adc1, 20, time::Duration::new(5, 0))?;
+        }
+//         let raw_current_reading =
+//          powered_adc1.read(&mut cts[0].current_pin.pin).unwrap() - DC_CURRENT[ac_phase];
+        //         let current_reading =
+        //             CURRENT_SCALE[ac_phase] * ((raw_current_reading as f32 * 3.3) / 4095.0);
+        //
+        //         let raw_voltage_reading =
+        //             powered_adc1.read(&mut single_phase_voltage_pin).unwrap() - DC_VOLTAGE[ac_phase];
+        //         let voltage_reading =
+        //             VOLTAGE_SCALE[ac_phase] * ((raw_voltage_reading as f32 * 3.3) / 4095.0);
+        //
+        //         current_samples[sample_count] = current_reading;
+        //         voltage_samples[sample_count] = voltage_reading;
+        //
+        //         sample_count += 1;
+        //
+        //         info!("Current: {}", current_reading);
+        //         info!("Voltage: {}", voltage_reading);
+        //
+        //         if sample_count >= MAX_SAMPLES {
+        //             let current_rms = calc_rms(&current_samples, sample_count);
+        //             let voltage_rms = calc_rms(&voltage_samples, sample_count);
+        //             sample_count = 0;
+        //             info!("Current RMS : {}", current_rms);
+        //             info!("Voltage RMS : {}", voltage_rms);
+        //         }
+        //
+        sleep(Duration::from_millis(1000));
+    }
+}
 
-        let raw_voltage_reading =
-            powered_adc1.read(&mut single_phase_voltage_pin).unwrap() - DC_VOLTAGE[ac_phase];
-        let voltage_reading =
-            VOLTAGE_SCALE[ac_phase] * ((raw_voltage_reading as f32 * 3.3) / 4095.0);
+fn init_adc(pins: Pins) -> anyhow::Result<[CT; AC_PHASE as usize]> {
+    #[cfg(feature = "single-phase")]
+    {
+        Ok([CT {
+            current_pin: CurrentPin {
+                pin: pins.gpio35.into_analog_atten_11db()?,
+                ical: 3.3,
+                offset_i: 1000,
+            },
+            voltage_pin: VoltagePin {
+                pin: pins.gpio34.into_analog_atten_11db()?,
+                vcal: 3.3,
+                phase_cal: 1.2,
+                offset_v: 1000,
+            },
+            sample_count: 0,
+            current_samples: [0.0; MAX_SAMPLES],
+            voltage_samples: [0.0; MAX_SAMPLES],
+        }])
+    }
+    #[cfg(feature = "three-phase")]
+    {
+        Ok([
+            CT {
+                current_pin: CurrentPin {
+                    pin: pins.gpio32.into_analog_atten_11db()?,
+                    ical: 3.3,
+                    offset_i: 1000,
+                },
+                voltage_pin: VoltagePin {
+                    pin: pins.gpio39.into_analog_atten_11db()?,
+                    vcal: 3.3,
+                    phase_cal: 1.2,
+                    offset_v: 1000,
+                },
+                sample_count: 0,
+                current_samples: [0.0; MAX_SAMPLES],
+                voltage_samples: [0.0; MAX_SAMPLES],
+            },
+            CT {
+                current_pin: CurrentPin {
+                    pin: pins.gpio35.into_analog_atten_11db()?,
+                    ical: 3.3,
+                    offset_i: 1000,
+                },
+                voltage_pin: VoltagePin {
+                    pin: pins.gpio36.into_analog_atten_11db()?,
+                    vcal: 3.3,
+                    phase_cal: 1.2,
+                    offset_v: 1000,
+                },
+                sample_count: 0,
+                current_samples: [0.0; MAX_SAMPLES],
+                voltage_samples: [0.0; MAX_SAMPLES],
+            },
+            CT {
+                current_pin: CurrentPin {
+                    pin: pins.gpio34.into_analog_atten_11db()?,
+                    ical: 3.3,
+                    offset_i: 1000,
+                },
+                voltage_pin: VoltagePin {
+                    pin: pins.gpio33.into_analog_atten_11db()?,
+                    vcal: 3.3,
+                    phase_cal: 1.2,
+                    offset_v: 1000,
+                },
+                sample_count: 0,
+                current_samples: [0.0; MAX_SAMPLES],
+                voltage_samples: [0.0; MAX_SAMPLES],
+            },
+        ])
+    }
+}
 
-        current_samples[sample_count] = current_reading;
-        voltage_samples[sample_count] = voltage_reading;
-
-        sample_count += 1;
-
-        info!("Current: {}", current_reading);
-        info!("Voltage: {}", voltage_reading);
-
-        if sample_count >= MAX_SAMPLES {
-            let current_rms = calc_rms(&current_samples, sample_count);
-            let voltage_rms = calc_rms(&voltage_samples, sample_count);
-            sample_count = 0;
-            info!("Current RMS : {}", current_rms);
-            info!("Voltage RMS : {}", voltage_rms);
+impl CT {
+    fn calculate_energy(
+        &mut self,
+        powered_adc1: &mut PoweredAdc<ADC1>,
+        crossing: u32,
+        timeout: time::Duration,
+    ) -> anyhow::Result<()> {
+        let supply_voltage = 3.3;
+        let mut start = time::Instant::now(); // start.elapsed() makes sure it doesnt get stuck in the loop if there is an error.
+        let start_v = 0.0;
+        // 1) Waits for the waveform to be close to 'zero' (mid-scale adc) part in sin curve.
+        loop {
+            if (start_v < ADC_COUNT as f32 * 0.55) && (start_v > ADC_COUNT as f32 * 0.45) {
+                break;
+            }
+            if start.elapsed() > timeout {
+                break;
+            }
         }
 
-        sleep(Duration::from_millis(1000));
+        // 2) Main measurement loop
+        let mut cross_count = 0;
+        let mut n_samples = 0;
+
+        // Used for delay/phase compensation
+        let mut filtered_v = 0.0;
+        let mut last_filtered_v = 0.0;
+        let mut filtered_i = 0.0;
+
+        let mut phase_shift_v = 0.0;
+
+        let mut sample_v: u16 = 0;
+        let mut sample_i: u16 = 0;
+        let mut offset_v: f32 = 0.0;
+        let mut offset_i: f32 = 0.0;
+
+        let (mut sq_v, mut sum_v, mut sq_i, mut sum_i, mut inst_p, mut sum_p) =
+            (0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
+        let (mut last_v_cross, mut check_v_cross) = (true, true);
+
+        start = time::Instant::now();
+        while (cross_count < crossing) && (start.elapsed() > timeout) {
+            // A) Read in raw voltage and current samples
+            sample_i = powered_adc1
+                .read(&mut self.current_pin.pin)
+                .unwrap_or(sample_i);
+            sample_v = powered_adc1
+                .read(&mut self.voltage_pin.pin)
+                .unwrap_or(sample_v);
+
+            // B) Apply digital low pass filters to extract the 2.5 V or 1.65 V dc offset,
+            //     then subtract this - signal is now centred on 0 counts.
+            offset_i = offset_i + ((sample_i as f32 - offset_i) / 1024.0);
+            filtered_i = sample_i as f32 - offset_i;
+
+            offset_v = offset_v + ((sample_v as f32 - offset_v) / 1024.0);
+            filtered_v = sample_v as f32 - offset_v;
+
+            // C) RMS
+            sq_v = filtered_v * filtered_v;
+            sum_v += sq_v;
+
+            sq_i = filtered_i * filtered_i;
+            sum_i += sq_i;
+
+            // E) Phase calibration
+            phase_shift_v =
+                last_filtered_v + self.voltage_pin.phase_cal * (filtered_v - last_filtered_v);
+
+            // F) Instantaneous power calc
+            sum_p += phase_shift_v * filtered_i;
+
+            // G) Find the number of times the voltage has crossed the initial voltage
+            //    - every 2 crosses we will have sampled 1 wavelength
+            //    - so this method allows us to sample an integer number of half wavelengths which increases accuracy
+            last_v_cross = check_v_cross;
+            if sample_v as f32 > start_v {
+                check_v_cross = true;
+            } else {
+                check_v_cross = false;
+            }
+            if n_samples == 1 {
+                last_v_cross = check_v_cross;
+            }
+
+            if last_v_cross != check_v_cross {
+                cross_count += 1;
+            }
+        }
+
+        let v_ratio = self.voltage_pin.vcal * (3.3 / (ADC_COUNT as f32));
+        let v_rms = v_ratio * f32::sqrt(sum_v / n_samples as f32);
+
+        let i_ratio = self.current_pin.ical * (3.3 / (ADC_COUNT as f32));
+        let i_rms = i_ratio * f32::sqrt(sum_i / n_samples as f32);
+
+        // Calculate power values
+        let real_power = v_ratio * i_ratio * sum_p / n_samples as f32;
+        let apparent_power = v_rms * i_rms;
+        let power_factor = real_power / apparent_power;
+
+        Ok(())
     }
 }
 
@@ -251,7 +437,7 @@ fn init_access_point(
 }
 
 /// Initilizes the web server and registers some handlers.
-fn init_web_server(keystore: Arc<EspNvsStorage>) -> anyhow::Result<EspHttpServer> {
+fn init_web_server() -> anyhow::Result<EspHttpServer> {
     let mut server = EspHttpServer::new(&Default::default())?;
 
     server.handle_get("/", |req, mut res| {
